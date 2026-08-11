5b7da0f01cc4dfbffa089c4809b3286427ef57ca
Hemansh-X797 <kundansmishra@gmail.com>
2026-08-11 16:42:21 +0530

    hook(lenis): integrate smooth scroll provider and inertia tracking

---
+++ src/hooks/useLenis.ts
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.15,
+      easing: (t) => 1 - Math.pow(1 - t, 4),
+      smoothWheel: true,
+    });
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+}
